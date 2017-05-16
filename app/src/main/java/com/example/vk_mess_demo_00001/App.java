77bdaf641aebe187044775cd91c7a101cce86eda
matek3022 <matek3022@gmail.com>
2017-05-16 21:00:12 +0400

    some fitures

--- app/src/main/java/com/example/vk_mess_demo_00001/App.java
+++ app/src/main/java/com/example/vk_mess_demo_00001/App.java
@@ -2,9 +2,12 @@ package com.example.vk_mess_demo_00001;
 
 import android.app.Application;
 
-import com.example.vk_mess_demo_00001.SQLite.DBHelper;
-import com.example.vk_mess_demo_00001.Utils.VKService;
+import com.example.vk_mess_demo_00001.managers.PreferencesManager;
+import com.example.vk_mess_demo_00001.sqlite.DBHelper;
+import com.example.vk_mess_demo_00001.utils.VKService;
 import com.facebook.drawee.backends.pipeline.Fresco;
+
+import java.util.ArrayList;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -14,6 +17,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public class App extends Application {
+    public static final ArrayList<Integer> frwdMessages = new ArrayList<>();
     public static final Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("https://api.vk.com/method/")
             .addConverterFactory(GsonConverterFactory.create())
@@ -24,5 +28,6 @@ public class App extends Application {
         super.onCreate();
         Fresco.initialize(getApplicationContext());
         DBHelper.init(getApplicationContext());
+        PreferencesManager.init(getApplicationContext());
     }
 }
