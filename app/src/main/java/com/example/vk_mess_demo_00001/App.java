4ee61fb8c4128d79d0363459633f10c393560e7b
matek3022 <matek3022@gmail.com>
2017-05-16 21:04:27 +0400

    refactor

--- app/src/main/java/com/example/vk_mess_demo_00001/App.java
+++ app/src/main/java/com/example/vk_mess_demo_00001/App.java
@@ -2,8 +2,8 @@ package com.example.vk_mess_demo_00001;
 
 import android.app.Application;
 
+import com.example.vk_mess_demo_00001.SQLite.DBHelper;
 import com.example.vk_mess_demo_00001.managers.PreferencesManager;
-import com.example.vk_mess_demo_00001.sqlite.DBHelper;
 import com.example.vk_mess_demo_00001.utils.VKService;
 import com.facebook.drawee.backends.pipeline.Fresco;
 
